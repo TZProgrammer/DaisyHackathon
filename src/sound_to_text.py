c2d3eb72958ca1eeddeb5779ce8fbad16e37fe13
Thomas Zeger <thomaszeger@gmail.com>
2023-01-14 19:17:47 -0500

    pooling now allows simultaneous recording and transcription

--- src/sound_to_text.py
+++ src/sound_to_text.py
@@ -1,3 +1,6 @@
+import os
+import time
+
 import whisper
 
 
@@ -9,12 +12,19 @@ def main():
     model = whisper.load_model("base")
     data_stream_path = "../data/recorded_data/record_stream.mp3"
 
-    transcription = transcribe(data_stream_path, model)
-    print(transcription["text"])
+    prev_mod_time = -1
+    start = time.time()
+    while time.time() - start < 35:
+        time.sleep(0.2)
+        if(os.stat(data_stream_path).st_mtime != prev_mod_time):
+            prev_mod_time = os.stat(data_stream_path).st_mtime  
+            transcription = transcribe(data_stream_path, model)
+            print(transcription["text"])
+            print()
 
-    # Save transcription
-    with open(f"../results/stream/transcription.txt", "w") as out_transcription:
-        out_transcription.write(transcription["text"])
+            # Save transcription
+            with open(f"../results/stream/transcription.txt", "w") as out_transcription:
+                out_transcription.write(transcription["text"])
 
 
 if __name__ == "__main__":

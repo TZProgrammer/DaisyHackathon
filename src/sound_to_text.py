4919be22aa6ad27ccb2b44d01f5b81a482bd5ba9
Thomas Zeger <thomaszeger@gmail.com>
2023-01-15 02:08:52 -0500

    Sentiment analysis works perfectly

--- src/sound_to_text.py
+++ src/sound_to_text.py
@@ -12,12 +12,15 @@ def main():
     model = whisper.load_model("base")
     data_stream_path = "../data/recorded_data/record_stream.mp3"
 
+    if os.path.exists(data_stream_path) and os.path.isfile(data_stream_path):
+        os.remove(data_stream_path)
+
     prev_mod_time = -1
     start = time.time()
     max_time = 100
     while time.time() - start < max_time:
         time.sleep(0.2)
-        if(os.stat(data_stream_path).st_mtime != prev_mod_time):
+        if(os.path.exists(data_stream_path) and os.stat(data_stream_path).st_mtime != prev_mod_time):
             prev_mod_time = os.stat(data_stream_path).st_mtime  
             transcription = transcribe(data_stream_path, model)
             print(transcription["text"])
